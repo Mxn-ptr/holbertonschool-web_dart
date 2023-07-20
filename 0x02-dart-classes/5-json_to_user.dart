b9eb2a8dee24f980974113c0b2820c75ddae2b2f
Mxn-ptr <maxence0@hotmail.fr>
2023-07-20 14:58:06 +0200

    Fix output

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -25,7 +25,7 @@ class User {
 	}
 
 	String toString() {
-		return 'User(id: ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height})';
+		return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height})';
 	}
 }
 
