1d24dd72d08feab4c8f8dd7a8958fcbf38498ef6
Mxn-ptr <maxence0@hotmail.fr>
2023-07-20 09:36:39 +0200

    Add task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,20 @@
+class Password {
+	String _password = '';
+	Password({required String password}) {
+		this._password = password;
+	}
+
+	bool isValid() {
+		return (
+			this._password.length >= 8 &&
+			this._password.length <= 16 &&
+			this._password.contains(RegExp(r'[A-Z]')) &&
+			this._password.contains(RegExp(r'[a-z]')) &&
+			this._password.contains(RegExp(r'[0-9]'))
+		);
+	}
+
+	String toString() {
+		return "Your Password is: ${this._password}";
+	}
+}
